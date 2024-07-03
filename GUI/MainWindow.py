06a69afbda2adec5a6078848db1e739cfaabb0d4
Lin <a21guobaotete@yeah.net>
2024-07-03 23:21:55 +0800

    上传识别都没问题

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -6,52 +6,56 @@ from main import API
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1200, 1000)
+        MainWindow.setGeometry(500,500,1500,1100)
+        MainWindow.setStyleSheet("background-color: white;")
+        # 设置 centralwidget
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(29, 10, 791, 531))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout.setObjectName("gridLayout")
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
 
-        #按钮：上传
-        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.pushButton.setObjectName("pushButton")
-        self.horizontalLayout.addWidget(self.pushButton)
-        spacerItem1 = QtWidgets.QSpacerItem(1, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem1)
-
-        #按钮：识别
-        self.pushButton_2 = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.horizontalLayout.addWidget(self.pushButton_2)
-        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem2)
-        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
-
-        #展示图片
-        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        # 展示图片的标签
+        self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setText("")
-        self.label.setPixmap(QtGui.QPixmap("../pictures/1.jpg"))
+        self.label.setPixmap(QtGui.QPixmap("../pictures/111.jpg"))
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        # 设置标签位置和大小
+        self.label.setGeometry(100, 60, 700,650)
 
-        #展示识别结果
-        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
+
+        # 展示识别结果的标签
+        # 创建标签 label_2 并设置位置和大小
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setObjectName("label_2")
-        self.gridLayout.addWidget(self.label_2, 0, 2, 2, 1)
+        self.label_2.setGeometry(880, 60, 500, 1000)
 
-        #其他
+        # 设置标签的背景颜色和文字大小
+        self.label_2.setStyleSheet("background-color: #AB9B93; color: white; font-size: 26px;")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+
+        # 上传按钮
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        # 设置按钮位置和大小
+        self.pushButton.setGeometry(150, 840, 200, 160)
+        self.pushButton.setStyleSheet(
+            "QPushButton { background-color:#0D98BA; color: white; font-size: 40px; "
+            "font-weight: bold;border-radius: 50%;font-family: '微软雅黑';}")
+
+        # 识别按钮
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        # 设置按钮位置和大小
+        self.pushButton_2.setGeometry(490, 840, 200, 160)
+        self.pushButton_2.setStyleSheet(
+            "QPushButton { background-color:#0D98BA; color: white; font-size: 40px; "
+            "font-weight: bold;border-radius: 50%;font-family: '微软雅黑';}")
+
+
+        # 其他
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 835, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, MainWindow.width(), 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -66,7 +70,7 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "上传"))
         self.pushButton_2.setText(_translate("MainWindow", "识别"))
-        self.label_2.setText(_translate("MainWindow", "识别结果"))
+        self.label_2.setText(_translate("MainWindow",""))
 
 
 
@@ -76,7 +80,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.open_child_window)
-
+        self.ui.pushButton_2.clicked.connect(self.APIGet)
+        self.uploaded_file_path = None  # 类变量来存储文件路径
 
     def open_child_window(self):
         self.child1 = QFileWidget()
@@ -84,12 +89,21 @@ class MainWindow(QtWidgets.QMainWindow):
         # 连接信号到槽函数
         self.child1.image_uploaded.connect(self.handle_image_uploaded)
 
+    def APIGet(self):
+        if self.uploaded_file_path:  # 确保文件路径已设置
+            formatted_json = API(self.uploaded_file_path)
+            self.ui.label_2.setText(formatted_json)
+
+
     def handle_image_uploaded(self, file_path):
-        print(file_path)
+        self.uploaded_file_path = file_path  # 存储文件路径
         pixmap = QtGui.QPixmap(file_path)
         self.ui.label.setPixmap(pixmap.scaled(self.ui.label.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation))
+
+    def handle_answer_show(self,file_path):
         formatted_json = API(file_path)
         self.ui.label_2.setText(formatted_json)
+
 
 
 
