c042ce6f178ad5858bac2561f25e54f8d5092cba
Lin <a21guobaotete@yeah.net>
2024-07-04 16:11:26 +0800

    害怕，存一下

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -3,17 +3,18 @@ from FileUploda import QFileWidget
 from main import api
 import sys
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.setGeometry(500, 500, 1500, 1100)
-        MainWindow.setStyleSheet("background-color: white;")
-        # 设置 centralwidget
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+class MainUseWindow(QtWidgets.QWidget):
 
-        # 展示图片的标签
-        self.label = QtWidgets.QLabel(self.centralwidget)
+    def __init__(self):
+        super().__init__()
+        self.setupUi()
+
+    def setupUi(self):
+        self.setObjectName("MainWindow")
+        self.setGeometry(500, 500, 1500, 1100)
+        self.setStyleSheet("background-color: white;")
+
+        self.label = QtWidgets.QLabel(self)
         self.label.setText("")
         self.label.setPixmap(QtGui.QPixmap("../pictures/111.jpg"))
         self.label.setScaledContents(True)
@@ -23,16 +24,15 @@ class Ui_MainWindow(object):
 
         # 展示识别结果的标签
         # 创建标签 label_2 并设置位置和大小
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2 = QtWidgets.QLabel(self)
         self.label_2.setObjectName("label_2")
         self.label_2.setGeometry(880, 60, 500, 1000)
 
         # 设置标签的背景颜色和文字大小
         self.label_2.setStyleSheet("background-color: #AB9B93; color: white; font-size: 26px;")
-        MainWindow.setCentralWidget(self.centralwidget)
 
         # 上传按钮
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton = QtWidgets.QPushButton(self)
         self.pushButton.setObjectName("pushButton")
         # 设置按钮位置和大小
         self.pushButton.setGeometry(150, 840, 200, 160)
@@ -41,7 +41,7 @@ class Ui_MainWindow(object):
             "font-weight: bold;border-radius: 50%;font-family: '微软雅黑';}")
 
         # 识别按钮
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2 = QtWidgets.QPushButton(self)
         self.pushButton_2.setObjectName("pushButton_2")
         # 设置按钮位置和大小
         self.pushButton_2.setGeometry(490, 840, 200, 160)
@@ -49,35 +49,21 @@ class Ui_MainWindow(object):
             "QPushButton { background-color:#0D98BA; color: white; font-size: 40px; "
             "font-weight: bold;border-radius: 50%;font-family: '微软雅黑';}")
 
-        # 其他
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, MainWindow.width(), 22))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.pushButton.clicked.connect(self.open_child_window)
+        self.pushButton_2.clicked.connect(self.api_get)
+        self.uploaded_file_path = None  # 类变量来存储文件路径
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "上传"))
         self.pushButton_2.setText(_translate("MainWindow", "识别"))
         self.label_2.setText(_translate("MainWindow", ""))
 
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.open_child_window)
-        self.ui.pushButton_2.clicked.connect(self.api_get)
-        self.uploaded_file_path = None  # 类变量来存储文件路径
 
     def open_child_window(self):
         self.child = QFileWidget()
@@ -88,23 +74,20 @@ class MainWindow(QtWidgets.QMainWindow):
     def api_get(self):
         if self.uploaded_file_path:  # 确保文件路径已设置
             formatted_json = api(self.uploaded_file_path)
-            self.ui.label_2.setText(formatted_json)
+            self.label_2.setText(formatted_json)
 
     def handle_image_uploaded(self, file_path):
         self.uploaded_file_path = file_path  # 存储文件路径
         pixmap = QtGui.QPixmap(file_path)
-        self.ui.label.setPixmap(
-            pixmap.scaled(self.ui.label.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation))
+        self.label.setPixmap(
+            pixmap.scaled(self.label.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation))
 
     def handle_answer_show(self, file_path):
         formatted_json = api(file_path)
-        self.ui.label_2.setText(formatted_json)
-
+        self.label_2.setText(formatted_json)
 
 if __name__ == "__main__":
-
-
     app = QtWidgets.QApplication(sys.argv)
-    mainWindow = MainWindow()
+    mainWindow = MainUseWindow()
     mainWindow.show()
     sys.exit(app.exec_())
---
+++ main.py
@@ -0,0 +1,82 @@
+import json
+import requests
+import base64
+import cv2 as cv
+
+
+def api(path):
+    # 百度AI开放平台车辆型号识别API的URL
+    request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/vehicle_detect"  # 指定用于车辆型号识别的API端点
+
+    # 打开并读取图片文件，准备进行编码
+    f = open(path, 'rb')  # 以二进制读模式打开指定路径的图片文件
+    img = base64.b64encode(f.read()).decode('utf-8')  # 将图片内容读取后编码为Base64字符串，并解码为UTF-8格式的字符串
+    f.close()  # 关闭文件
+
+    # 构造请求参数
+    params = {"image": img, "top_num": 3}  # 将Base64编码的图片和期望返回的最可能车型数量作为参数
+    access_token = '24.2fcfd71dd6da73a65e54de62af80c4cd.2592000.1722495145.282335-89990893'  # 百度AI服务的访问令牌
+    # 将access_token添加到请求URL中，这是进行API请求的必要认证步骤
+    request_url = request_url + "?access_token=" + access_token
+
+    # 设置请求头
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'}  # 发送Base64编码的图片，保持'application/x-www-form-urlencoded'
+
+    # 发送POST请求
+    response = requests.post(request_url, data=params, headers=headers)  # 发送POST请求
+
+    # 处理响应
+    if response.status_code == 200:  # 检查响应状态码是否为200，表示请求成功
+        json_data = response.json()  # 将响应内容解析为JSON格式
+        formatted_json = json.dumps(json_data, indent=4,
+                                    ensure_ascii=False)  # 将JSON数据格式化为易读的字符串，包括中文（通过设置ensure_ascii=False）
+        print(formatted_json)  # 打印美化后的JSON数据
+    else:
+        print(f"Error: {response.status_code}, {response.text}")  # 如果请求失败，打印错误状态码和错误信息
+    return formatted_json
+
+
+def process_image(file_path):
+    request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/vehicle_detect"
+
+    # 读取并编码图像
+    with open(file_path, 'rb') as f:
+        img_bytes = f.read()
+        img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+
+    # 发送请求
+    params = {"image": img_base64, "top_num": 3}
+    access_token = '24.2fcfd71dd6da73a65e54de62af80c4cd.2592000.1722495145.282335-89990893'
+    request_url = request_url + "?access_token=" + access_token
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    response = requests.post(request_url, data=params, headers=headers)
+    data = response.json()
+
+    # 加载原始图像
+    img = cv.imread(file_path)
+
+    # 处理车辆检测结果
+    num = data['vehicle_num']['car']
+    for item in data['vehicle_info']:
+        location = item['location']
+        x1 = location['left']
+        y1 = location['top']
+        x2 = x1 + location['width']
+        y2 = y1 + location['height']
+        cv.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        text = item['type']
+        position = (x1, y1 - 2)
+        font = cv.FONT_HERSHEY_SIMPLEX
+        font_scale = 1
+        color = (0, 0, 255)
+        thickness = 2
+        cv.putText(img, text, position, font, font_scale, color, thickness, cv.LINE_AA)
+
+    # 显示图像
+    cv.imshow('Image Window', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+process_image('pictures/1.jpg')
